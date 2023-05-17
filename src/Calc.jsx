8ab36a24ab14949763d7e2d8f39dad6fcfb02e9f
Kirill Zhiganov <zhigano_k.n@mail.ru>
2023-05-17 16:34:21 +0300

    Настроил полностью рабочий калькулятор

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -1,24 +1,86 @@
 import style from './CalcStyle.module.css';
+import { useState } from 'react';
+import * as func from './modules/changeFunc';
+import { getResult } from './modules/changeFunc';
+
+const calculatorButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 export const Calc = () => {
-  const calculatorButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const [result, setResult] = useState(0);
+  const [symbolFlag, setSymbolFlag] = useState('');
+  const [actionFlag, setActionFlag] = useState('');
+  const [count, setCount] = useState(0);
+  const [styleResult, setStyleResult] = useState(style.fieldResult);
 
   return (
     <>
-      <div className={style.container}>
-        <div className={style.fieldResult}>100</div>
+      <div className={`${style.container}`}>
+        <div className={styleResult}>{result}</div>
         <div className={style.calcContainer}>
           {calculatorButtons.map((button) => (
-            <button className={style.buttonCalc} key={button}>
+            <button
+              className={style.buttonCalc}
+              key={button}
+              onClick={() => {
+                func.changeFieldResult(
+                  button,
+                  result,
+                  setResult,
+                  actionFlag,
+                  setActionFlag,
+                  setStyleResult,
+                );
+              }}
+            >
               {button}
             </button>
           ))}
-          <button className={style.resultNow}>=</button>
+          <button
+            className={style.resultNow}
+            onClick={() => {
+              getResult(count, result, setResult, symbolFlag);
+              setCount(0);
+              setSymbolFlag('');
+              func.actionResult(actionFlag, setActionFlag);
+              setStyleResult(`${style.fieldResult} ${style.active}`);
+            }}
+          >
+            =
+          </button>
         </div>
         <div className={style.actionButton}>
-          <button>+</button>
-          <button>-</button>
-          <button>C</button>
+          <button
+            onClick={() => {
+              if (symbolFlag === '') {
+                setSymbolFlag('+');
+                func.actionResult(actionFlag, setActionFlag);
+                setCount(count + result);
+              }
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              if (symbolFlag === '') {
+                setSymbolFlag('-');
+                func.actionResult(actionFlag, setActionFlag);
+                setCount(count + result);
+              }
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={() => {
+              setSymbolFlag('');
+              setResult(0);
+              setCount(0);
+              setStyleResult(style.fieldResult);
+            }}
+          >
+            C
+          </button>
         </div>
       </div>
     </>
