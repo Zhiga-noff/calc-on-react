51c20edac4bfc1d6fd022a28ea8d69341bf6e2e4
Kirill Zhiganov <zhiganov_k.n@mail.ru>
2023-05-20 18:10:43 +0300

    все рендерю из массива

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import * as func from './modules/changeFunc';
 import { getResult } from './modules/changeFunc';
 
-const calculatorButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const calculatorButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+','-','C','='];
 
 export const Calc = () => {
   const [result, setResult] = useState(0);
@@ -12,75 +12,83 @@ export const Calc = () => {
   const [count, setCount] = useState(0);
   const [styleResult, setStyleResult] = useState(style.fieldResult);
 
+  function clickToResultButton() {
+      getResult(count, result, setResult, symbolFlag);
+      setCount(0);
+      setSymbolFlag('');
+      func.actionResult(actionFlag, setActionFlag);
+      setStyleResult(`${style.fieldResult} ${style.active}`);
+    }
+
+    function clickToResetButton() {
+        setSymbolFlag('');
+        setResult(0);
+        setCount(0);
+        setStyleResult(style.fieldResult);
+    }
+
   return (
     <>
       <div className={`${style.container}`}>
         <div className={styleResult}>{result}</div>
         <div className={style.calcContainer}>
-          {calculatorButtons.map((button) => (
-            <button
-              className={style.buttonCalc}
-              key={button}
-              onClick={() => {
-                func.changeFieldResult(
-                  button,
-                  result,
-                  setResult,
-                  actionFlag,
-                  setActionFlag,
-                  setStyleResult,
-                );
-              }}
-            >
-              {button}
-            </button>
-          ))}
-          <button
-            className={style.resultNow}
-            onClick={() => {
-              getResult(count, result, setResult, symbolFlag);
-              setCount(0);
-              setSymbolFlag('');
-              func.actionResult(actionFlag, setActionFlag);
-              setStyleResult(`${style.fieldResult} ${style.active}`);
-            }}
-          >
-            =
-          </button>
+          {calculatorButtons.map((button) => {
+              if(!isNaN(button)) {
+                  console.log( Number(button));
+                  return <button
+                      className={style.buttonCalc}
+                      key={button}
+                      onClick={() => {
+                          func.changeFieldResult(
+                              button,
+                              result,
+                              setResult,
+                              actionFlag,
+                              setActionFlag,
+                              setStyleResult,
+                          );
+                      }}
+                  >
+                      {button}
+                  </button>
+              }
+              if (button === '=') {
+                  return <button
+                      className={style.resultNow}
+                      onClick={clickToResultButton}
+                  >
+                      {button}
+                  </button>
+              }
+          })}
+
         </div>
         <div className={style.actionButton}>
-          <button
-            onClick={() => {
-              if (symbolFlag === '') {
-                setSymbolFlag('+');
-                func.actionResult(actionFlag, setActionFlag);
-                setCount(count + result);
-              }
-            }}
-          >
-            +
-          </button>
-          <button
-            onClick={() => {
-              if (symbolFlag === '') {
-                setSymbolFlag('-');
-                func.actionResult(actionFlag, setActionFlag);
-                setCount(count + result);
-              }
-            }}
-          >
-            -
-          </button>
-          <button
-            onClick={() => {
-              setSymbolFlag('');
-              setResult(0);
-              setCount(0);
-              setStyleResult(style.fieldResult);
-            }}
-          >
-            C
-          </button>
+            {calculatorButtons.map((button) => {
+                if(button === '+' || button === '-') {
+                    return <button
+                        className={style.buttonCalc}
+                        key={button}
+                        onClick={() => {
+                            if (symbolFlag === '') {
+                                setSymbolFlag(button);
+                                func.actionResult(actionFlag, setActionFlag);
+                                setCount(count + result);
+                            }
+                        }}
+                    >
+                        {button}
+                    </button>
+                }
+                if (button === 'C') {
+                    return <button
+                        className={style.resultNow}
+                        onClick={clickToResetButton}
+                    >
+                        {button}
+                    </button>
+                }
+            })}
         </div>
       </div>
     </>
